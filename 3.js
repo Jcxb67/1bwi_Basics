76c72c8fdade3eaec26b1e304fb4cc7e318f96b7
Jxcb67 <jacketwitch1x@gmail.com>
2025-11-20 12:09:17 +0100

    RAN Test

---
+++ 3.js
@@ -0,0 +1,13 @@
+//Variablen
+
+let ApplePricePerKilo = 3.43;
+let BananaPricePerKilo = 2.41;
+
+let AppleAverage = 0.34;
+let BanaAverage = 0.22;
+
+console.log("8 Äpfel: " + Math.round(ApplePricePerKilo * 8 * AppleAverage));
+
+console.log("17 Bananen: " + Math.round(BananaPricePerKilo * 17 * BanaAverage));
+
+console.log
