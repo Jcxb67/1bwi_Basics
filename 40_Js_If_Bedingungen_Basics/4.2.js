c2a789f6249d656673be17d78b274a9e91928653
Jxcb67 <jacketwitch1x@gmail.com>
2025-11-27 10:37:33 +0100

    4.1.js and 4.2.js Done

---
+++ 40_Js_If_Bedingungen_Basics/4.2.js
@@ -0,0 +1,13 @@
+let trafficLight = "yellow";
+
+if (trafficLight == "green") {
+    console.log("You may go");
+
+} else if (trafficLight == "yellow") {
+    console.log("Caution!");
+
+} else if (trafficLight == "red") {
+    console.log("Stop!");
+}
+
+
