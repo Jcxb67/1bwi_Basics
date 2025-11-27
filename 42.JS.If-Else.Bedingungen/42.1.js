bb7ba2ea46a160383c578caf268fd4c748b00624
Jxcb67 <jacketwitch1x@gmail.com>
2025-11-27 10:57:06 +0100

    42.1.js Done

---
+++ 42.JS.If-Else.Bedingungen/42.1.js
@@ -0,0 +1,15 @@
+
+let randomNumber = Math.floor(Math.random() * 10);
+
+console.log(" Die erzeugte Zahl ist: " + randomNumber);
+
+if (randomNumber < 5) {
+    console.log("The number is less than 5");
+
+} else if (randomNumber > 5) {
+    console.log("The number is greater than 5");
+
+} else {  
+    console.log("The number is exactly 5");
+
+}
